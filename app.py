648e22d7b86824f9072dd1d1728433624a71a790
SID12g <sid12g@naver.com>
2024-02-18 21:54:12 +0900

    deffff

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-data = requests.get('https://finance.naver.com/item/main.naver?code=005930')
-soup = BeautifulSoup(data.content, 'html.parser')
+def price(code):
+    data = requests.get('https://finance.naver.com/item/sise.naver?code='+code)
+    soup = BeautifulSoup(data.content, 'html.parser')
+    return soup.select('strong#_nowVal')[0].text
 
-dd = soup.find_all('em', class_="no_down")[0].text
-dd2 = soup.find_all('td', class_="f_down")[0].text
-dd3 = soup.select('td.f_down em')[0].text
-imge = soup.select('img#img_chart_area')[0]
-print(imge['src'])
+print('samsung: ',price('005930'))
+print('lg: ',price('003555'))
 
-link = requests.get('https://finance.naver.com/item/main.naver?code=003555')
-soup = BeautifulSoup(link.content, 'html.parser')
-dd = soup.find_all('em', class_="no_up")[0].text
-print(dd)
+# data = requests.get('https://finance.naver.com/item/main.naver?code=005930')
+# soup = BeautifulSoup(data.content, 'html.parser')
+
+# dd = soup.find_all('em', class_="no_down")[0].text
+# dd2 = soup.find_all('td', class_="f_down")[0].text
+# dd3 = soup.select('td.f_down em')[0].text
+# imge = soup.select('img#img_chart_area')[0]
+# print(imge['src'])
+
+# link = requests.get('https://finance.naver.com/item/main.naver?code=003555')
+# soup = BeautifulSoup(link.content, 'html.parser')
+# dd = soup.find_all('em', class_="no_up")[0].text
+# print(dd)
